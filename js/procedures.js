17ff845a5afa611907f5d694413dbff1151fe4f7
RogerCorb <rogeriocgprest@gmail.com>
2024-08-04 10:51:58 -0300

    update landingpage

--- js/procedures.js
+++ js/procedures.js
@@ -4,6 +4,7 @@ const nome = document.getElementById("nome");
 const email = document.getElementById("email");
 const telefone = document.getElementById("telefone");
 const descricao = document.getElementById("message");
+const sectionContact = document.getElementById("section3")
 
 
 formulario.addEventListener('submit', (event) => {
@@ -24,8 +25,9 @@ formulario.addEventListener('submit', (event) => {
 function sendForm() {
 
     formulario.reset();
-    contato_form.classList.remove('contato_form_conteudo')
-    contato_form.classList.add('apresentacao_conteudo_obrigado')
+    
+    //contato_form.classList.remove('contato_form_conteudo')
+    contato_form.classList.toggle('apresentacao_conteudo_obrigado')
     contato_form.innerHTML = `<div>
      <h2>Agradecemos por seu contato. Em breve retornaremos a sua solicitação</h2>
      <h3>Redirecionamento em: <span id="contador-segundos">6</span> seg</h3>
@@ -44,8 +46,25 @@ function sendForm() {
         }
     }, 1000);
 
-    setInterval(() => {   
-        window.location.href = "contato.html";
+    const count = setInterval(() => {  
+        clearInterval(count); 
+        //contato_form.classList.remove('apresentacao_conteudo_obrigado');
+        contato_form.classList.toggle('contato_form_conteudo'); 
+        document.getElementById("section3").innerHTML = sectionContact;       
+        window.location.href = "#section1";
     }, 6000);
     return
 }
+
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+  
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+  
+      targetElement.scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+});
