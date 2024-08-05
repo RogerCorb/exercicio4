ce4d484f830a51003b589234fb7e64980204e67b
RogerCorb <rogeriocgprest@gmail.com>
2024-08-05 06:41:43 -0300

    refactor

--- js/procedures.js
+++ js/procedures.js
@@ -4,36 +4,25 @@ const nome = document.getElementById("nome");
 const email = document.getElementById("email");
 const telefone = document.getElementById("telefone");
 const descricao = document.getElementById("message");
-const sectionContact = document.getElementById("section3")
-
 
 formulario.addEventListener('submit', (event) => {
-    event.preventDefault();
 
-    if (nome.value === '' || telefone.value === '' || email.value === ''  ) { 
-        document.getElementById('message').innerText = 'Todos os campos são obrigatórios !';
-        setInterval(() => {   
-            window.location.href = "contato.html";
-        }, 3000);
-        
-    } else { 
-        sendForm();
-    }
+    event.preventDefault();    
+    sendForm();
     
 });
 
 function sendForm() {
 
-    formulario.reset();
+    formulario.reset();    
     
-    //contato_form.classList.remove('contato_form_conteudo')
     contato_form.classList.toggle('apresentacao_conteudo_obrigado')
     contato_form.innerHTML = `<div>
      <h2>Agradecemos por seu contato. Em breve retornaremos a sua solicitação</h2>
-     <h3>Redirecionamento em: <span id="contador-segundos">6</span> seg</h3>
+     <h3>Redirecionamento em: <span id="contador-segundos">8</span> seg</h3>
     </div>`;
 
-    let segundos = 6;
+    let segundos = 8;
 
     document.querySelector('span').style.color = 'black';      
    
